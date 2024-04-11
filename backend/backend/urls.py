149b5d4cf10b57f57d08f8d871be9ebc61100caf
James Prentice <jamesprenticez@gmail.com>
2024-04-12 10:12:41 +1200

    BE - notes view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
   path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
   path("api/refresh/", TokenRefreshView.as_view(), name="refresh"),
   path("api-auth/", include("rest_framework.urls")),
+  path("api/", include("api.urls")),
 ]

f4bd79b7646b8ca3712512f93897b7d4ce217e63
James Prentice <jamesprenticez@gmail.com>
2024-04-15 11:32:33 +1200

    Button shine

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import { Paths } from '@models';
 import { Button } from '@components/ui';
+import { ButtonShine } from '@components/ui/ButtonShine';
 
 const Home = () => {
   return (
@@ -11,8 +12,41 @@ const Home = () => {
           User Settings
         </Button>
       </NavLink>
+
+      <div>
+
+
+<ButtonShine variant='filled' color="info">
+ test
+</ButtonShine>
+      </div>
+
+
+      {/* <div className="bg-[url('shine.png')] bg-no-repeat bg-[center_left_-256px] bg-green-500 flex cursor-pointer hover:bg-[center_left_256px] hover:transition-[background] hover:ease-out hover:duration-1000 p-4 rounded-md">
+        test
+      </div> */}
+
     </div>
   )
 }
 
 export default Home;
+
+// a.logo .shine {
+//   width: 256px;
+// 	height: 256px;
+// 	display: block;
+// 	background: url("https://i.imgur.com/n1CetrS.png") -256px 0 no-repeat;	
+//   -webkit-transition: background 0s linear;
+//   -moz-transition: background 0s linear;
+//   -ms-transition: background 0s linear;
+//   -o-transition: background 0s linear;
+// }
+
+// a.logo:hover .shine {
+// 	background-position: 256px 0px;
+//   -webkit-transition: background 1s ease-out;
+//   -moz-transition: background 1s ease-out;
+//   -ms-transition: background 1s ease-out;
+//   -o-transition: background 1s ease-out;
+// }

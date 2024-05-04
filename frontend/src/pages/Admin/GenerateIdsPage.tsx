9cd92cf9e441cbb8527e61284dc41cc031440b27
James Prentice <jamesprenticez@gmail.com>
2024-05-04 17:56:06 +1200

    FE - date picker returns an updated value

--- frontend/src/pages/Admin/GenerateIdsPage.tsx
+++ frontend/src/pages/Admin/GenerateIdsPage.tsx
@@ -1,18 +1,33 @@
 import { DatePicker, Label } from '@components/ui';
-import React from 'react'
+import { DatePicker2 } from '@components/ui/DatePicker2';
+import React, {useState} from 'react'
 
 function GenerateIdsPage() {
-  const dateNow = new Date().toISOString();
+  const dateNow = new Date(1993, 6, 17)
+
+  const [selectedDate, setSelectedDate] = useState(dateNow.toString());
+
+  const handleDateSelection = (date: string) => {
+    setSelectedDate(date);
+  };
   
   return (
     <div >
-      {/* <section>{date.toISOString()}</section> */}
       <section className='border border-major/50 ronded-md p-4'>
         <Label value="Start Date:" >
-
-        <DatePicker />
+          <DatePicker />
         </Label>
-        {dateNow}
+
+        <Label value="End Date:">
+          <DatePicker2 
+            initialDate={selectedDate}
+            updateDate={(date: string) => handleDateSelection(date)}
+          />
+        </Label>
+
+        <h1>{dateNow.toString()}</h1>
+        <h1>{selectedDate}</h1>
+
       </section>
     </div>
   )
